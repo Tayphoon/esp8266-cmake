b69c3cce1f5ffbbd4f51e3d2ef04608385dfc4a6
Tayphoon <Tayphoon@Tayphoons-MacBook-Pro.local>
2017-04-01 01:21:46 +0300

    Add RTOS sdk, nonos 2.0

--- sdk/rtos-1.5.0.cmake
+++ sdk/rtos-1.5.0.cmake
@@ -1,24 +1,29 @@
 if(CMAKE_HOST_SYSTEM_NAME MATCHES "Linux")
-    set(ESP8266_SDK_BASE ${USER_HOME}/git/esp-open-sdk/sdk CACHE PATH "Path to the ESP8266 SDK")
+    set(ESP8266_SDK_BASE ${USER_HOME}/git/ESP8266_RTOS_SDK CACHE PATH "Path to the ESP8266 SDK")
 elseif(CMAKE_HOST_SYSTEM_NAME MATCHES "Windows")
-    set(ESP8266_SDK_BASE ${USER_HOME}/dev/projects/esp-open-sdk CACHE PATH "Path to the ESP8266 SDK")
+    set(ESP8266_SDK_BASE ${USER_HOME}/dev/projects/ESP8266_RTOS_SDK CACHE PATH "Path to the ESP8266 SDK")
 elseif(CMAKE_HOST_SYSTEM_NAME MATCHES "Darwin")
-    set(ESP8266_SDK_BASE /Volumes/ESPToolchain/sdk/ CACHE PATH "Path to the ESP8266 SDK")
+    set(ESP8266_SDK_BASE /Volumes/ESPToolchain/ESP8266_RTOS_SDK CACHE PATH "Path to the ESP8266 SDK")
 else()
     message(FATAL_ERROR "Unsupported build platforom.")
 endif()
 
-
 if (ESP8266_FLASH_SIZE MATCHES "512K")
     set_target_properties(firmware PROPERTIES
-        LINK_FLAGS "-L${ESP8266_SDK_BASE}/ld -Teagle.app.v6.ld"
-    )
+            LINK_FLAGS "-L${ESP8266_SDK_BASE}/ld -Teagle.app.v6.ld"
+            )
     set(FW_ADDR_1 0x00000)
     set(FW_ADDR_2 0x40000)
 elseif (ESP8266_FLASH_SIZE MATCHES "1M")
     set_target_properties(firmware PROPERTIES
-        LINK_FLAGS "-L${ESP8266_SDK_BASE}/ld -Teagle.app.v6.new.1024.app1.ld"
-    )
+            LINK_FLAGS "-L${ESP8266_SDK_BASE}/ld -Teagle.app.v6.new.1024.app1.ld"
+            )
+    set(FW_ADDR_1 0x00000)
+    set(FW_ADDR_2 0x01010)
+elseif (ESP8266_FLASH_SIZE MATCHES "2M")
+    set_target_properties(firmware PROPERTIES
+            LINK_FLAGS "-L${ESP8266_SDK_BASE}/ld -Teagle.app.v6.new.2048.ld"
+            )
     set(FW_ADDR_1 0x00000)
     set(FW_ADDR_2 0x01010)
 else()
@@ -26,16 +31,20 @@ else()
 endif()
 
 target_include_directories(ESP8266_SDK INTERFACE
-    ${ESP8266_SDK_BASE}/include
-    ${ESP8266_SDK_BASE}/include/json
-)
+        ${ESP8266_SDK_BASE}/include
+        ${ESP8266_SDK_BASE}/include/espressif
+        ${ESP8266_SDK_BASE}/extra_include
+        ${ESP8266_SDK_BASE}/includefreertos
+        ${ESP8266_SDK_BASE}/include/json
+        ${ESP8266_SDK_BASE}/include/lwip
+        ${ESP8266_SDK_BASE}/include/lwip/ipv4
+        ${ESP8266_SDK_BASE}/include/lwip/ipv6
+        )
 
-find_library(ESP8266_SDK_LIB_AT at ${ESP8266_SDK_BASE}/lib)
 find_library(ESP8266_SDK_LIB_CRYPTO crypto ${ESP8266_SDK_BASE}/lib)
 find_library(ESP8266_SDK_LIB_ESPNOW espnow ${ESP8266_SDK_BASE}/lib)
 find_library(ESP8266_SDK_LIB_JSON json ${ESP8266_SDK_BASE}/lib)
 find_library(ESP8266_SDK_LIB_LWIP lwip ${ESP8266_SDK_BASE}/lib)
-find_library(ESP8266_SDK_LIB_MAIN main ${ESP8266_SDK_BASE}/lib)
 find_library(ESP8266_SDK_LIB_MESH mesh ${ESP8266_SDK_BASE}/lib)
 find_library(ESP8266_SDK_LIB_NET80211 net80211 ${ESP8266_SDK_BASE}/lib)
 find_library(ESP8266_SDK_LIB_PHY phy ${ESP8266_SDK_BASE}/lib)
@@ -47,23 +56,21 @@ find_library(ESP8266_SDK_LIB_WPA wpa ${ESP8266_SDK_BASE}/lib)
 find_library(ESP8266_SDK_LIB_WPS wps ${ESP8266_SDK_BASE}/lib)
 
 target_link_libraries(ESP8266_SDK INTERFACE
-    gcc
-    ${ESP8266_SDK_LIB_AT}
-    ${ESP8266_SDK_LIB_CRYPTO}
-    ${ESP8266_SDK_LIB_ESPNOW}
-    ${ESP8266_SDK_LIB_JSON}
-    ${ESP8266_SDK_LIB_LWIP}
-    ${ESP8266_SDK_LIB_MAIN}
-    ${ESP8266_SDK_LIB_MESH}
-    ${ESP8266_SDK_LIB_NET80211}
-    ${ESP8266_SDK_LIB_PHY}
-    ${ESP8266_SDK_LIB_PP}
-    ${ESP8266_SDK_LIB_PWM}
-    ${ESP8266_SDK_LIB_SMARTCONFIG}
-    ${ESP8266_SDK_LIB_SSL}
-    ${ESP8266_SDK_LIB_WPA}
-    ${ESP8266_SDK_LIB_WPS}
-)
+        gcc
+        ${ESP8266_SDK_LIB_CRYPTO}
+        ${ESP8266_SDK_LIB_ESPNOW}
+        ${ESP8266_SDK_LIB_JSON}
+        ${ESP8266_SDK_LIB_LWIP}
+        ${ESP8266_SDK_LIB_MESH}
+        ${ESP8266_SDK_LIB_NET80211}
+        ${ESP8266_SDK_LIB_PHY}
+        ${ESP8266_SDK_LIB_PP}
+        ${ESP8266_SDK_LIB_PWM}
+        ${ESP8266_SDK_LIB_SMARTCONFIG}
+        ${ESP8266_SDK_LIB_SSL}
+        ${ESP8266_SDK_LIB_WPA}
+        ${ESP8266_SDK_LIB_WPS}
+        )
 
 add_custom_target(
     firmware_binary ALL
@@ -73,7 +80,6 @@ add_custom_target(
 
 get_directory_property(extra_clean_files ADDITIONAL_MAKE_CLEAN_FILES)
 set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES "${extra_clean_files};${PROJECT_SOURCE_DIR}/firmware/firmware_${FW_ADDR_1}.bin;${PROJECT_SOURCE_DIR}/firmware/firmware_${FW_ADDR_2}.bin")
-#set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES "firmware_${FW_ADDR_1}.bin firmware_${FW_ADDR_2}.bin")
 
 add_dependencies(firmware_binary firmware)
 
